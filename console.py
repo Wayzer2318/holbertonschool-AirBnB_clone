82a9e9b1d60badf1f2a12bb83c1453180d9d9d26
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-24 11:02:01 +0100

    adding update function

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ import models
 from models.user import User
 from models.base_model import BaseModel
 import sys
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -44,7 +45,6 @@ class HBNBCommand(cmd.Cmd):
         """ print string of all instance or just a class """
         strings = args.split()
         new_list = []
-        address_value = 0
         if len(strings) == 1:
             if strings[0] not in HBNBCommand.class_dict.keys():
                 print("** class doesn't exist **")
@@ -60,6 +60,33 @@ class HBNBCommand(cmd.Cmd):
             for key, value in models.storage.all().items():
                 new_list.append(str(models.storage.all()[key]))
             print(new_list)
+
+    def do_update(self, args):
+        """ Update an instance """
+        strings = shlex.split(args)
+        models.storage.reload()
+        new_dict = models.storage.all()
+        if len(strings) == 0:
+            print("** class name missing **")
+        elif strings[0] not in HBNBCommand.class_dict.keys():
+            print("** class doesn't exist **")
+        elif len(strings) == 1:
+            print("** instance id missing **")
+        elif strings[0] + '.' + strings[1] not in new_dict.keys():
+            print("** no instance found **")
+        elif len(strings) == 2:
+            print("** attribute name missing **")
+        elif len(strings) == 3:
+            print("** value missing **")
+        else:
+            key = strings[0] + '.' + strings[1]
+            if hasattr(new_dict[key], strings[2]):
+                caster = type(getattr(new_dict[key], strings[2]))
+                setattr(new_dict[key], strings[2], caster(strings[3]))
+                models.storage.save()
+            else:
+                setattr(new_dict[key], strings[2], strings[3])
+                models.storage.save()
 
     def do_destroy(self, args):
         """ delete an instance """
