501ad0bc7daf16a83c544c8be233672eb19746f2
Wayzer2318 <5820@holbertonstudents.com>
2023-07-05 22:30:30 +0200

    fix init

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,26 @@ import datetime
 create base model
 """
 class BaseModel():
-    def __init__(self):
-        id = str(uuid.uuid4())
-        created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
+    """
+    base model class
+    """
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    value = datetime.datetime.strptime(value, 
+                                                       "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
+        else:
+            id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
         __str__
         """
-        print("{} {} {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("{} {} {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """
@@ -35,3 +45,4 @@ class BaseModel():
                 dict[x] = y.isoformat()
             else:
                 dict[x] = y
+        return dic
