b9a9233483cc97e139dfb75581ddf80310267938
Wayzer2318 <5820@holbertonstudents.com>
2023-07-07 13:39:59 +0200

    adding user

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" class User """
+
+
+class User(BaseModel):
+    """
+    Class User 
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
