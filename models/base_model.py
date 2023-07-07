df1f03b99b5045390a789af40940a390ed1c2567
Wayzer2318 <5820@holbertonstudents.com>
2023-07-07 10:42:16 +0200

    updating save with storage

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -33,6 +34,7 @@ class BaseModel:
         save
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
