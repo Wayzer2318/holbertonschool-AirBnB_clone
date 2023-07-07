8444f8125f51ccfa710007af008107bd6665bb17
Wayzer2318 <5820@holbertonstudents.com>
2023-07-07 10:37:43 +0200

    adding a condition

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,23 @@
 #!usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 
-class BaseModel():
+class BaseModel:
     """
     base model class
     """
     def __init__(self, *args, **kwargs):
+        """ initialisaation  """
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     timeval = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if k != "__class__":
                     setattr(self, k, timeval)
+                elif k != "__class__":
+                    setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
